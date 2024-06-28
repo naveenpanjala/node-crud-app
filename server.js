80964c6d920301cc92a8e93a06cd16f88babe691
naveenpanjala <naveenkumar12345k@gmail.com>
2024-06-29 01:27:46 +0530

    a

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-const PORT = process.env.PORT || 3000;
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
