473c028d9436e495b4eb144be7eaabc79f51582d
naveenpanjala <naveenkumar12345k@gmail.com>
2024-06-29 00:43:41 +0530

    added env variables

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -15,6 +16,8 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log("listening on port 3000");
 });
